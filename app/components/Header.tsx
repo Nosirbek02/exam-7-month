7020dd9f4b31cc17b0c6118d4d40919d9dd5252d
Nosirbek02 <axmadjonovnosirbek@gmail.com>
2023-09-27 20:47:31 +0500

    restart

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,35 +8,7 @@ import axios from "axios";
 
 
 export const Header = () => {
-  const router = window.location;
-  const [isPath, setIsPath] = useState("/");
-  const [isLogout, setIsLogout] = useState(false);
-  useEffect(() => {
-    axios
-      .post(
-        "http://207.154.221.44:4002/api/dishes",
-        {},
-        { headers: { Authorization: localStorage.getItem("token") } }
-      )
-      .then((res) => {
-        if (res.data.statusCode === 500) {
-          console.log("is logout");
-          setIsLogout(true);
-        } else {
-          setIsLogout(false);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    if (router.pathname.endsWith("/")) {
-      setIsPath("/");
-    } else if (router.pathname.endsWith("/dishes")) {
-      setIsPath("/dishes");
-    } else if (router.pathname.endsWith("/payment")) {
-      setIsPath("/payment");
-    }
-  }, [router.pathname, localStorage.getItem("token")]);
+  
   return (
     <header
       className="pt-12 pr-24 pl-24 pb-20"
@@ -48,27 +20,19 @@ export const Header = () => {
             eatly
           </p>
 
-          <Link className="text-lg capitalize font-medium mr-14 " href="/" style={{color: `${isPath === "/" ? "#6C5FBC" : "#606060"}`, fontFamily: "Inter",}}>
+          <Link className="text-lg capitalize font-medium mr-14 " href="/" >
             Home
           </Link>
-          <Link className="text-lg capitalize font-medium mr-14 " href="/dishes" style={{color: `${isPath === "/dishes" ? "#6C5FBC" : "#606060"}`, fontFamily: "Inter",}}>
+          <Link className="text-lg capitalize font-medium mr-14 " href="/dishes" >
             Dishes
           </Link>
         </div>
 
         <div className="flex items-center mb-6 ">
           <Link href="/payment">
-            <Shoppingicon color={isPath === "/payment" ? "#6C5FBC" : undefined}/>
+            <Shoppingicon />
           </Link>
-          {isLogout ? (
-            <button
-              onClick={() => {
-                localStorage.removeItem("token");
-              }}
-            >
-              Logout
-            </button>
-          ) : (
+          
             <>
               <Link className="text-lg font-bold capitalize ml-11 mr-9 " href="/page/login" style={{color: "#606060",fontFamily: "Inter",}}>
                 Login
@@ -78,7 +42,7 @@ export const Header = () => {
                 Sign up
               </Link>
             </>
-          )}
+          
         </div>
       </div>
     </header>
